b437fe2e9046ab4f33d0f4ce0d30eb2d61032c32
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-05-26 09:24:52 -0300

    Adicionando Django REST Framework para fornecer API para o frontend

--- backend/olap/clientes_zodb/services/zodb_service.py
+++ backend/olap/clientes_zodb/services/zodb_service.py
@@ -1,13 +1,14 @@
 from ZODB import FileStorage, DB
 from BTrees.OOBTree import OOBTree
 import transaction
-from zodb_models.jogo import Jogo
-from zodb_models.usuario import Usuario
+
+from clientes_zodb.models.zodb_models import Jogo
+from clientes_zodb.models.zodb_models import Usuario
 
 
 class BaseDB():
     def __init__(self):
-        self.storage = FileStorage.FileStorage('jogos.fs')
+        self.storage = FileStorage.FileStorage('zodb/jogos.fs')
         self.db = DB(self.storage)
         self.connection = self.db.open()
         self.root = self.connection.root()
@@ -26,12 +27,15 @@ class JogoDB(BaseDB):
         if not hasattr(self.root, 'jogos'):
             self.root.jogos = OOBTree()
 
+        if not hasattr(self.root, 'usuarios'):
+            self.root.usuarios = OOBTree()
+
     # Métodos para jogo
     def criar_jogo(self, jogo: Jogo):
         self.root.jogos[jogo.id] = jogo
         transaction.commit()
 
-    def buscar_jogo(self, id) -> Jogo:
+    def buscar_jogo_id(self, id) -> Jogo:
         return self.root.jogos.get(id)
     
     def listar_jogos(self) -> list[Jogo]:
@@ -42,7 +46,7 @@ class JogoDB(BaseDB):
         transaction.commit()
     
     def excluir_jogo(self, id: int) -> Jogo:
-        jogo = self.buscar_jogo(id)
+        jogo = self.buscar_jogo_id(id)
         if jogo:
             del self.root.jogos[jogo.id]
             transaction.commit()
@@ -74,15 +78,4 @@ class JogoDB(BaseDB):
             del self.root.usuarios[usuario.id]
             transaction.commit()
             return usuario
-
-     
-# class UsuarioDB(BaseDB):
-#     def __init__(self):
-#         super().__init__()
-
-#         # Inicializa as "tabelas" se não existirem
-#         if not hasattr(self.root, 'usuarios'):
-#             self.root.usuarios = OOBTree()
-
-
-    
+        
