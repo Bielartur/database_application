f003f448be4bc44a187b32133e97decd124c08ff
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-05-26 15:01:54 -0300

    ETL funcionando

--- backend/clientes_zodb/views/jogo_view.py
+++ backend/clientes_zodb/views/jogo_view.py
@@ -2,10 +2,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from clientes_zodb.services.zodb_service import JogoDB, get_jogo_db
+from clientes_zodb.services.zodb_service import get_jogo_db
 from clientes_zodb.models.zodb_models import Jogo
 from clientes_zodb.utils.indentificar_id import identificar_novo_id
 from etl.etl_runner import main as etl_main  # Adicionado import do ETL
+import transaction
 
 
 class JogoListCreate(APIView):
@@ -39,16 +40,17 @@ class JogoListCreate(APIView):
                     "ano": jogo.ano,
                     "categoria": jogo.categoria,
                     "duracao": jogo.duracao,
-                    "preco": jogo.preco
+                    "preco": jogo.preco,
+                    "url_imagem": jogo.url_imagem
                 }
                 for jogo in jogos
             ]
-            if not data:
-                return Response(
-                    {'mensagem': 'Nenhum jogo cadastrado ainda'},
-                    status=status.HTTP_200_OK
-                )
-            return Response(data, status=status.HTTP_200_OK)
+        if not data:
+            return Response(
+                {'mensagem': 'Nenhum jogo cadastrado ainda'},
+                status=status.HTTP_200_OK
+            )
+        return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request):     
         novo_id = identificar_novo_id('Jogo', db)
@@ -65,11 +67,12 @@ class JogoListCreate(APIView):
         )
         with get_jogo_db() as db:
             db.criar_jogo(jogo)
-        # Executa o ETL após criar o jogo
-        try:
-            etl_main()
-        except Exception as e:
-            print(f"Erro ao rodar ETL: {e}")
+            # Executa o ETL após criar o jogo
+            try:
+                etl_main(db=db)
+            except Exception as e:
+                print(f"Erro ao rodar ETL: {e}")
+            transaction.commit()
         return Response({"mensagem": "Jogo criado", "id": novo_id,"dados": dados}, status=status.HTTP_201_CREATED)
     
     def put(self, request, jogo_id):
@@ -91,30 +94,32 @@ class JogoListCreate(APIView):
 
             db.atualizar_jogo(jogo)  # você pode usar commit aqui também
 
-        # Executa o ETL após atualizar o jogo
-        try:
-            etl_main()
-        except Exception as e:
-            print(f"Erro ao rodar ETL: {e}")
+            # Executa o ETL após atualizar o jogo
+            try:
+                etl_main(db=db)
+            except Exception as e:
+                print(f"Erro ao rodar ETL: {e}")
+            transaction.commit()
         return Response({"mensagem": "Jogo atualizado"}, status=200)
     
     def delete(self, request, jogo_id):
         with get_jogo_db() as db:
             jogo = db.excluir_jogo(jogo_id)
-        dados = {
-                "id": jogo.id,
-                "titulo": jogo.titulo,
-                "descricao": jogo.descricao,
-                "ano": jogo.ano,
-                "categoria": jogo.categoria,
-                "duracao": jogo.duracao,
-                "preco": jogo.preco
-        }
-        if jogo is None:
-            return Response({"erro": "Jogo não encontrado"}, status=404)
-        # Executa o ETL após deletar o jogo
-        try:
-            etl_main()
-        except Exception as e:
-            print(f"Erro ao rodar ETL: {e}")
+            dados = {
+                    "id": jogo.id,
+                    "titulo": jogo.titulo,
+                    "descricao": jogo.descricao,
+                    "ano": jogo.ano,
+                    "categoria": jogo.categoria,
+                    "duracao": jogo.duracao,
+                    "preco": jogo.preco
+            }
+            if jogo is None:
+                return Response({"erro": "Jogo não encontrado"}, status=404)
+            # Executa o ETL após deletar o jogo
+            try:
+                etl_main(db=db)
+            except Exception as e:
+                print(f"Erro ao rodar ETL: {e}")
+            transaction.commit()
         return Response({"mensagem": "Jogo deletado", "dados": dados}, status=204)
